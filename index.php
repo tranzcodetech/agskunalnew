49c6792bc173c2218aabb08890f16da31136f3ac
tranzcodetech <tranzcodetech@gmail.com>
2020-11-23 23:35:25 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,24 @@
+
+    <h1>
+</br>
+</br>
+</h1>
+
+<?php
+
+$title = "THIS IS THE  HOME PAGE OF AGRAGAMEE SANGHA";
+include 'header.php';
+
+    echo' <p>I am directed to inform you that since remaining portion of  
+     restoration work in Mahananda Main Canal will be started soon after Puja vacation, 
+      so discharge through MMC will be discontinued on and from the 5th November, 2020 until further decision <p/>';
+
+
+?>
+<button type="button" class="btn btn-primary">SUBMIT</button>
+
+
+<?php
+include 'footer.php';
+
+?>
